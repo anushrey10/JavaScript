ca7b771b72b01b1f5def43b805922266629046b7
Anushrey Shubham <114229619+anushrey10@users.noreply.github.com>
2024-08-31 08:08:50 +0000

    variables and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 22052443
+let accountEmail = "22052443@kiit.ac.in"
+var accountPassword = "Anu@983556"
+accountCity = "Bhubaneswar"
+let accountState
+
+//accountId = 2
+
+accountEmail = "anushreyshubham@icloud.com"
+accountPassword = "anushrey983556"
+accountCity = "Jamshedpur"
+
+console.table([accountId,accountEmail,accountPassword,accountPassword,accountState])
+
+// prefer not to use var , use let instead
+//this will not put you in block scope trouble
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello")
+//alert(3+3)
+console.log(3+3);
+
+//tc39.es & nvm for documentation
+
+//DATA TYPES
+//number => 2 to power 32
+//bigint => bigger than number
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+//typeof => shows data type 
+
+console.log(typeof null);// type of null is OBJECT
+console.log(typeof undefined);
