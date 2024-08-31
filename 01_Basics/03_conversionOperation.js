05195583913f6488ad93788cd7b17981f7fd7ac0
Anushrey Shubham <114229619+anushrey10@users.noreply.github.com>
2024-08-31 08:25:54 +0000

    type conversion done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Anushrey"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true =>1 , false =>0
+
+let isLoggedIn = 0
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(BooleanisLoggedIn);
+//1 => true , 0 => false
+//"" => false , "abcd" => true
+
+let someNumber = 99
+
+let StringSomeNumber = String(someNumber)
+
+console.log(StringSomeNumber);
+console.log(typeof StringSomeNumber);
