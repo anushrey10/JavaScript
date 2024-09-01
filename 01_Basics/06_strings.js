8eb34acd05021efa999af4a2aa07f5a0d8971c24
Anushrey Shubham <114229619+anushrey10@users.noreply.github.com>
2024-09-01 13:57:46 +0000

    strings

---
+++ 01_Basics/06_strings.js
@@ -0,0 +1,19 @@
+const name = "Anushrey Shubham"
+let roll = 22052443
+
+console.log(`My name is ${name} and my roll number is ${roll}`);
+
+const gameName = new String('AnushreY')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+const newString = gameName.substring(0,5)
+console.log(newString);
+
+
